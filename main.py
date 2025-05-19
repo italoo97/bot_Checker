31e7904f872f199e7bfae92445f7ae435ca159d0
italoo97 <italohugodf39@gmail.com>
2025-05-19 12:08:15 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"✅ Bot conectado como {bot.user}")
+    print("🔍 Servidores encontrados:")
+
+    for guild in bot.guilds:
+        print(f"- {guild.name} | ID: {guild.id}")
+
+    await bot.close()  # Encerra após listar
+
+bot.run("SEU_TOKEN_DO_BOT")
